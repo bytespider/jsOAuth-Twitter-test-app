4301a2bf38dc0752546267d8acfc94eb5ff3e5a4
Rob Griffiths <rob@bytespider.eu>
2011-10-10 21:49:48 +0100

    added timeline test

--- Resources/spec/mainSpec.js
+++ Resources/spec/mainSpec.js
@@ -2,11 +2,12 @@ var consumerKey;
 var consumerSecret;
 
 describe("Twitter", function() {
+    var twitter = new OAuth.Twitter({
+        consumerKey: consumerKey,
+        consumerSecret: consumerSecret
+    });
+    
     it('should authenticate a user', function () {
-        var twitter = new OAuth.Twitter({
-            consumerKey: consumerKey,
-            consumerSecret: consumerSecret
-        });
         
         var successCallback = jasmine.createSpy('success_a');
         var failureCallback = jasmine.createSpy('failure_a');
@@ -22,27 +23,33 @@ describe("Twitter", function() {
             expect(failureCallback).not.toHaveBeenCalled();
         });
     });
-    /*
-    waits(3000);
+   
+    it("should show the user is logged in", function () {
+        expect(twitter.signedIn()).toBeTruthy();
+    });
+   
+    it("should fetch the user's home timeline", function () {
+        var result;
+        var app = {
+            success: function (data) {
+                result = data;
+            },
+            failure: function (data) {
+                result = data;
+            }
+        };
+        spyOn(app, 'success').andCallThrough();
+        spyOn(app, 'failure').andCallThrough();
         
-    it('should not authenticate a user', function () {
-        var twitter = new OAuth.Twitter({
-            consumerKey: consumerKey,
-            consumerSecret: consumerSecret
-        });
-        var successCallback = jasmine.createSpy('success_b');
-        var failureCallback = jasmine.createSpy('failure_b');
-        
-        twitter.authenticate(successCallback, failureCallback);
+        twitter.homeTimeline(null, app.success, app.failure);
         
         waitsFor(function () {
-            return successCallback.wasCalled || failureCallback.wasCalled;
-        }, "user to not be authenticated", 30000);
+            return app.success.callCount > 0 || app.failure.callCount > 0;
+        }, 60000);
         
         runs(function () {
-            expect(failureCallback).toHaveBeenCalled();
-            expect(successCallback).not.toHaveBeenCalled();
+            expect(app.success).toHaveBeenCalled();
+            expect(app.failure).not.toHaveBeenCalled();
         });
     });
-    */  
 });
